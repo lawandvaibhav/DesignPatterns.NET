03295ee323948299bcc0ad32d7cc739b1e06edc9
Vaibhav Lawand <lawand.vaibhav@gmail.com>
2022-08-12 21:09:59 +0530

    Added Design Pattern solution and Prism.Unity nuget package dependancy.

---
+++ Design Pattern Encyclopedia/Design Pattern Encyclopedia/UnityBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace Design_Pattern_Encyclopedia
+{
+    internal class UnityBootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
